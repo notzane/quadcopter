eabea08a3037bea4caeea3af7b79f04c42a9113a
Eclip5e5 <eseide@maret.org>
2014-08-29 20:38:56 -0400

    All the work from Friday-New more "efficient" code

---
+++ arduino/PID_IMU_Test_v2/PID_IMU_Test_v2.ino
@@ -0,0 +1,246 @@
+/*
+ * Ethan and Zane
+ * 8/29/14
+ * Quadcopter PID IMU v2
+ */
+ 
+//Includes
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "Wire.h"
+#include <PID_v1.h>
+#include <Servo.h>
+
+MPU6050 mpu;
+
+//MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+//Orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector       
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+//Declare PID vars for each axis
+double pSetpoint, pInput, pOutput;
+double rSetpoint, rInput, rOutput;
+
+//Input vars into PID algorithm
+PID pitchPID(&pInput, &pOutput, &pSetpoint, 2, 1, 0.1, DIRECT);
+PID rollPID(&rInput, &rOutput, &rSetpoint, 2, 1, 0.1, DIRECT);
+
+float pOffset;
+float rOffset;
+int absOffset;
+
+Servo brushless0;
+Servo brushless1;
+Servo brushless2;
+Servo brushless3;
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+    brushless0.attach(5);
+    brushless1.attach(6);
+    brushless2.attach(10);
+    brushless3.attach(11);
+    delay(10);
+    brushless0.write(40);
+    brushless1.write(40);
+    brushless2.write(40);
+    brushless3.write(40);
+    delay(2000);
+    Serial.begin(9600);
+    //Join I2C bus (I2Cdev library doesn't do this automatically)
+    Wire.begin();
+    TWBR = 24; //400kHz I2C clock (200kHz if CPU is 8MHz)
+    
+    //Initialize Devices
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    
+    
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+    
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+        
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    }
+    else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    } 
+    
+    pSetpoint = -0.23;
+    pInput = -0.23;
+    pitchPID.SetMode(AUTOMATIC);
+    pitchPID.SetOutputLimits(-100, 100);
+    
+    rSetpoint = -0.03;
+    rInput = -0.03;
+    rollPID.SetMode(AUTOMATIC);
+    rollPID.SetOutputLimits(-100, 100);
+}
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+    getypr();
+    pOffset = getPitchPID();
+    rOffset = getRollPID();
+    absOffset = motorVal() + 40;
+
+    //Serial.println(absOffset);
+    
+
+    
+//    Serial.println(rOffset);
+    
+}
+
+void getypr() {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {}
+    
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+    
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+    
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        //Serial.println(F("FIFO overflow!")); //Uncomment if you want it to print overflow
+        // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    }
+    else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        // display Euler angles in radians
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    }
+}
+
+float getPitchPID() {
+    pInput = ypr[1];
+    pitchPID.Compute();
+    return pOutput;
+}
+
+float getRollPID() {
+    rInput = ypr[2];
+    rollPID.Compute();
+    return rOutput;
+}
+
+String read_input() {
+    int index = 0;
+    char data[9];
+    while (Serial.available() <= 0 || Serial.read() != '{') { //actual progrm loop
+        getypr();
+        getypr();
+        pOffset = getPitchPID();
+        rOffset = getRollPID();
+        //Serial.print("r: "); Serial.print(rOffset);
+        //Serial.print("\t p: "); Serial.println(pOffset);
+        
+    brushless0.write(absOffset - pOffset + rOffset);
+    brushless1.write(absOffset - pOffset - rOffset +3);
+    brushless2.write(absOffset + pOffset - rOffset);
+    brushless3.write(absOffset + pOffset + rOffset);
+    }
+    delay(5);
+    while(Serial.available()>0) {
+        char input = Serial.read();
+        //Serial.println("char=" + input);
+        if (input == '}') {
+            data[index] = '\0';
+            return String(data);
+        }
+        data[index] = input;
+        index ++;
+        delay(4);
+    }
+    //data[index] = '\0';
+    
+    //Serial.print("data=");
+    //Serial.print(data);
+
+    //return String(data);
+}
+
+int motorVal() {
+    String val = read_input().substring(0,2);
+    return atoi(val.c_str());
+    //Serial.println(val);
+}
+
+
+    
+    
+        
+       
+
---
+++ processing/Abstract_Control_v1/Abstract_Control_v1.pde
@@ -0,0 +1,128 @@
+import processing.serial.*;
+
+Serial quadport;
+
+int throttle = 0;
+
+int forBack = 0;
+char fOrB = 'f';
+
+int lefRight = 0;
+char lOrR = 'l';
+
+void setup() {
+  size(640, 320);
+  textAlign(CENTER);
+
+  quadport = new Serial(this, "/dev/tty.usbserial-A501B6YR", 9600);
+}
+
+void draw() {
+  background(149);
+  
+  drawThrottle();
+  drawDirection(lefRight, forBack);
+}
+
+String send() {
+  String toSend = "{";
+  int ones = throttle%10;
+  int tens = throttle - ones; //In case the tens digit is a 0, makes sure to add a space for it
+  
+  //Adds throttle
+  toSend += str(tens/10);
+  toSend += str(ones);
+  //Adds forward/backwards
+  toSend += str(fOrB);
+  toSend += str(forBack);
+  //Adds left/right
+  toSend += str(lOrR);
+  toSend += str(lefRight);
+  //Adds end
+  toSend += "}";
+  return toSend;
+}
+
+void drawThrottle() {
+  fill(0);
+  noStroke();
+  rect(40, 60, 80, 200);
+   
+  fill(0, 128, 256);
+  rect(40, 260 - 2 * throttle, 80, 2 * throttle);
+  
+  fill(255);
+  textSize(20);
+  text(throttle, 80, 240);
+}
+
+void drawDirection(int x, int y) {
+  stroke(255);
+  line(380, 70, 380, 250);
+  line(290, 160, 470, 160);
+  
+  //Scale to graph
+  x *= 10;
+  y *= 10;
+  
+  if (fOrB == 'f') {
+    y *= -1;
+  }
+  if (lOrR == 'l') {
+    x *= -1;
+  }
+  stroke(255, 0, 0);
+  strokeWeight(3);
+  line(380, 160, 380 + x, 160 + y);
+}
+
+void keyPressed() {
+  //Direction
+  if (key == CODED) {
+    if (keyCode == UP) {
+      forBack++;
+    }
+    else if (keyCode == DOWN) {
+      forBack--;
+    }
+    else if (keyCode == LEFT) {
+      lefRight ++;
+    }
+    else if (keyCode == RIGHT) {
+      lefRight --;
+    }
+  }
+  else if (key == 'f') {
+    fOrB = 'f';
+  }
+  else if (key == 'b') {
+    fOrB = 'b';
+  }
+  else if (key == 'l') {
+    lOrR = 'l';
+  }
+  else if (key == 'r') {
+    lOrR = 'r';
+  }
+  //Throttle
+  else if (key == 'w') {
+    throttle += 1;
+  }
+  else if (key == 's') {
+    throttle -= 1;
+  }
+  //Limits range of throttle and direction
+  if (forBack > 9) {forBack = 9;}
+  else if (forBack < 0) {forBack = 0;}
+  
+  if (lefRight > 9) {lefRight = 9;}
+  else if (lefRight < 0) {lefRight = 0;}
+  
+  if (throttle > 99) {throttle = 99;}
+  else if (throttle < 0) {throttle = 0;}
+  //Sends through serial when a key is pressed
+  println(send());
+  quadport.write(send());
+}
+
+  
